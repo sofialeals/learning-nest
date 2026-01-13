5d6c356ab0561bd656b85555e25988a12538a3ce
sofialeals <156724109+sofialeals@users.noreply.github.com>
2026-01-13 09:00:34 -0300

    add typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: '',
+      host: '',
+      port: ,
+      username: '',
+      database: '',
+      password: '',
+      /*
+      autoLoadEntities: carrega entidades automaticamente,
+      sem necessidade de especificá-las unicamente
+      */ 
+      autoLoadEntities: true,
+      /*
+      synchronize: sincroniza as entidades com as tabelas
+      do banco de dados. NÃO DEVE SER USADO EM PRODUÇÃO, pois
+      pode provocar perda de dados
+      */ 
+      synchronize: true
+    }),
+    MessagesModule
+  ],
   controllers: [
     AppController
   ],
